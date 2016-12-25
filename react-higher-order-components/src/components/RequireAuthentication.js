20533bbdf2baf42e48fa8f22b774cb5a7ef7a2ca
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2016-12-25 17:06:33 -0500

    react-higher-order-component: example code.

--- react-higher-order-components/src/components/RequireAuthentication.js
+++ react-higher-order-components/src/components/RequireAuthentication.js
@@ -1,13 +1,33 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default function(ComposedComponent) {
-    class Authentication extends Component {
-        render() {
-           return (
-                return <ComposedComponent { ...this.props } />
-            )
-        }
+export default function (ComposedComponent) {
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    };
+
+    componentWillMount() {
+      if(!this.props.authenticated) {
+        this.context.router.push('/');
+      }
     }
 
-    return Authentication;
+    componentWillUpdate(nextProps) {
+      if(!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      return (
+        <ComposedComponent { ...this.props } />
+      );
+    }
+  }
+  function mapStateToProps(state) {
+    return {authenticated: state.authenticated};
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
--- react-higher-order-components/src/reducers/authentication.js
+++ react-higher-order-components/src/reducers/authentication.js
@@ -1,12 +1,11 @@
 import {CHANGE_AUTH} from '../actions/types';
 
-const authentication = function(state = {isAuthenticated: false}, action) {
+const authentication = function(state = false, action) {
     switch(action.type) {
         case CHANGE_AUTH:
             return action.payload;
         break;
     }
-
     return state;
 };
 export default authentication;
