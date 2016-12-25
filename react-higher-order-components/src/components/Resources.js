dd066ef765d0e682f6d76c37b55bdecd80b12946
Nazar Lesiv <nazarlesiv@Nazars-MBP.home>
2016-12-24 22:10:11 -0500

    react-higher-order-components: Initial Commit

---
+++ react-higher-order-components/src/components/Resources.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default () => {
+    return (
+        <div>
+            Super Special Recipe
+            <ul>
+                <li>Cup Sugar</li>
+                <li>Cup Pepper</li>
+                <li>Cup Salt</li>
+            </ul>
+        </div>
+    );
+}
---
+++ react-higher-order-components/src/reducers/authentication.js
@@ -0,0 +1,12 @@
+import {CHANGE_AUTH} from '../actions/types';
+
+const authentication = function(state = {isAuthenticated: false}, action) {
+    switch(action.type) {
+        case CHANGE_AUTH:
+            return action.payload;
+        break;
+    }
+
+    return state;
+};
+export default authentication;
